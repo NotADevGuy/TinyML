9f248b0f2578ec2c61030976c91ea35e9c3a7b7c
mattt <97069726+NotADevGuy@users.noreply.github.com>
2022-11-18 13:36:48 -0500

    t

---
+++ mainModel.py
@@ -0,0 +1,101 @@
+import cv2
+import tensorflow as tf
+
+cam = cv2.VideoCapture(0)
+cv2.namedWindow("May remove this.")
+
+img_counter = 0
+
+width = int(cam.get(3))
+height = int(cam.get(4))
+
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k % 256 == 27:  # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k % 256 == 32:  # SPACE pressed
+        img_name = f"opencv_frame_{img_counter}.png"
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+cv2.destroyAllWindows()
+
+
+"""
+# Playing video from file
+cap = cv2.VideoCapture('rackoon.mp4')
+
+# Default resolutions of the frame are obtained.The default resolutions are system dependent.
+# We convert the resolutions from float to integer.
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+# Define the codec and create VideoWriter object.The output is stored in 'output.avi' file.
+out = cv2.VideoWriter(FILE_OUTPUT, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
+                      10, (frame_width, frame_height))
+
+with detection_graph.as_default():
+    with tf.Session(graph=detection_graph) as sess:
+        # Definite input and output Tensors for detection_graph
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+        # Each box represents a part of the image where a particular object was detected.
+        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+        while(cap.isOpened()):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+
+            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+            image_np_expanded = np.expand_dims(frame, axis=0)
+
+            # Actual detection.
+            (boxes, scores, classes, num) = sess.run(
+                [detection_boxes, detection_scores, detection_classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
+
+            # Visualization of the results of a detection.
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                frame,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                category_index,
+                use_normalized_coordinates=True,
+                line_thickness=8)
+
+            if ret == True:
+                # Saves for video
+                out.write(frame)
+
+                # Display the resulting frame
+                cv2.imshow('Charving Detection', frame)
+
+                # Close window when "Q" button pressed
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+
+    # When everything done, release the video capture and video write objects
+    cap.release()
+    out.release()
+
+    # Closes all the frames
+    cv2.destroyAllWindows()"""
